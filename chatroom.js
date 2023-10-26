b43d1033ab88bffae80274ce8615cd420515fdc5
Preston Ford <preston.y.ford@gmail.com>
2023-10-25 23:35:37 -0600

    placeholder response

--- chatroom.js
+++ chatroom.js
@@ -1,3 +1,34 @@
+function create_other_message(author, text) {
+    const new_message = document.createElement("div");
+    new_message.classList.add("message");
+    new_message.classList.add("message-other");
+
+    const author_span = document.createElement("span");
+    author_span.classList.add("message-author");
+    author_span.innerText = author;
+    new_message.appendChild(author_span)
+    
+    const content_span = document.createElement("span");
+    content_span.innerText = text;
+    new_message.appendChild(content_span)
+    
+    return new_message;
+}
+
+function receive_message(message) {
+    document.getElementById("messages-container").appendChild(message);
+}
+
+// Placeholder function
+async function respond() {
+    min_respond_time = 300;
+    max_respond_time = 1400;
+    setTimeout(() => {
+        const new_message = create_other_message("Ki-Adi-Mundi", "What about the droid attack on the Wookiees?");
+        receive_message(new_message);
+    }, Math.random() * (max_respond_time - min_respond_time) + min_respond_time)
+}
+
 function create_self_message(text) {
     const new_message = document.createElement("div");
     new_message.classList.add("message");
@@ -16,8 +47,10 @@ function send_message() {
     const new_message = create_self_message(text_to_send);
     document.getElementById("messages-container").appendChild(new_message);
 
-    text_box.value = ""
-    text_box.focus()
+    text_box.value = "";
+    text_box.focus();
+
+    respond();
 }
 
 window.onload = function() {
